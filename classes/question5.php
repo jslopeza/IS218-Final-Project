f9a055cd3cd053cbd88f203a02f5918d3319d0d2
jslopeza <jasonlopeza95@gmail.com>
2014-12-08 15:36:48 -0500

    Updated text

--- classes/question5.php
+++ classes/question5.php
@@ -7,7 +7,7 @@
 			$this->content .= '
 			<div class="container">
 				<h2>Question 5<h2>
-				<h5>Create a web page that shows the colleges with the largest percentage increase in enrollment between the years of 2011 and 2010.</h5>
+				<h5>Create a web page that shows the colleges with the largest percentage increase in enrollment between the years of 2010 and 2011.</h5>
 			</div>';
 		}
 	}
