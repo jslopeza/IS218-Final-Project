a84fa482e654e160da6576d7113031b26005098b
jslopeza <jasonlopeza95@gmail.com>
2014-12-09 22:44:24 -0500

    Send  obj

--- classes/page.php
+++ classes/page.php
@@ -3,15 +3,16 @@
 	
 	abstract class page{
 		public $content;
+		public $db;
 		public function __construct(){
 			
 		}
 		public function __destruct(){
 			$this->heading();
-			$this->get();
+			//$this->get();
 			echo $this->content;
 		}
-		public function get(){
+		public function get($dbh){
 			
 		}
 		public function post(){
