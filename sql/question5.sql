feab84f3bd6e2fd49b96ef38c6e71091d82ab436
jslopeza <jasonlopeza95@gmail.com>
2014-12-10 16:47:35 -0500

    Spacing fix

--- sql/question5.sql
+++ sql/question5.sql
@@ -1,7 +1,1 @@
-SELECT colleges.INSTNM
-FROM colleges
-JOIN enrol_10 ON colleges.UNITID = enrol_10.UNITID
-JOIN enrol_11 ON colleges.UNITID = enrol_11.UNITID
-ORDER BY (
-enrol_10.EFYTOTLT / enrol_11.EFYTOTLT
-) *100 DESC 
+SELECT colleges.INSTNM FROM colleges JOIN enrol_10 ON colleges.UNITID = enrol_10.UNITID JOIN enrol_11 ON colleges.UNITID = enrol_11.UNITID ORDER BY (enrol_10.EFYTOTLT / enrol_11.EFYTOTLT) *100 DESC
