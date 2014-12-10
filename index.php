9a4be88f2020bd1dd45f53acb293c1ff1cdfaedf
jslopeza <jasonlopeza95@gmail.com>
2014-12-10 16:49:09 -0500

    Nav mobile friendly

--- index.php
+++ index.php
@@ -8,14 +8,25 @@
 	</head>
 	<body>
 		<nav class="navbar navbar-default" role="navigation">
-			<ul class="nav navbar-nav">
-				<li><a href="index.php">Home</a></li>
-				<li><a href="index.php?page=classes\question1">Question 1</a></li>
-				<li><a href="index.php?page=classes\question2">Question 2</a></li>
-				<li><a href="index.php?page=classes\question3">Question 3</a></li>
-				<li><a href="index.php?page=classes\question4">Question 4</a></li>
-				<li><a href="index.php?page=classes\question5">Question 5</a></li>
-			</ul>
+			<div class="navbar-header">
+		    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+		        </button>
+		      <a class="navbar-brand" href="#">IS218 Final</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="index.php?page=classes\question1">Question 1</a></li>
+					<li><a href="index.php?page=classes\question2">Question 2</a></li>
+					<li><a href="index.php?page=classes\question3">Question 3</a></li>
+					<li><a href="index.php?page=classes\question4">Question 4</a></li>
+					<li><a href="index.php?page=classes\question5">Question 5</a></li>
+				</ul>
+			</div>
 		</nav>
 		<?php
 			error_reporting(E_ALL);
