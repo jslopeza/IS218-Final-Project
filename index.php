03ab3a12986585bfb7fcc4c30a56c3f1bb019c09
jslopeza <jasonlopeza95@gmail.com>
2014-12-12 11:45:07 -0500

    Fixed navigation to responsive

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<body>
 		<nav class="navbar navbar-default" role="navigation">
 			<div class="navbar-header">
-		    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
 			        <span class="sr-only">Toggle navigation</span>
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
@@ -17,7 +17,7 @@
 		        </button>
 		      <a class="navbar-brand" href="index.php">IS218 Final</a>
 		    </div>
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		    <div class="collapse navbar-collapse" id="navbar">
 				<ul class="nav navbar-nav">
 					<li><a href="index.php?page=classes\question1">Question 1</a></li>
 					<li><a href="index.php?page=classes\question2">Question 2</a></li>
